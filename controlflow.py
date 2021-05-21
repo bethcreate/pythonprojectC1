8c06ca6b50d8494511f95e546451f7a2fe96dc87
bethcreate <kamaubetth@gmail.com>
2021-05-21 08:37:42 +0300

    first commit

---
+++ controlflow.py
@@ -0,0 +1,22 @@
+num = int(input("Enter a number: "))
+print("multiplication table of:")
+for i in range(1,11):
+    print(num,'x',i,'=',num*i)
+
+
+number = int(input("Enter a number: "))
+print("division table of: ")
+for a in range(1,11):
+    print(number,'/',a,'=',number/a)
+
+       
+
+
+
+
+
+
+
+
+
+
